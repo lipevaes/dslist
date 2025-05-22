37a5d3c46ccf7de302574a2e21b26900ad4e1603
Lipe Vaes <filipe.vaes10@gmail.com>
2025-05-22 11:04:21 -0300

    Domain model

---
+++ src/main/java/com/devvaes/dslist/services/GameService.java
@@ -0,0 +1,31 @@
+package com.devvaes.dslist.services;
+import java.util.List;
+
+import com.devvaes.dslist.dto.GameDTO;
+import com.devvaes.dslist.dto.GameMinDTO;
+import com.devvaes.dslist.entities.Game;
+import com.devvaes.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class GameService {
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+
+    }
+
+}
